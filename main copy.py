0d8b1f16edd8692607cee92d4f574140b2ec3a65
nicoleSpaul <spaulding.nicole@gmail.com>
2023-12-04 19:23:08 -0500

    works for join and create

--- main copy.py
+++ main copy.py
@@ -1,12 +1,12 @@
 import asyncio
 import websockets
+import random
 
 
 #128.113.139.63:5000
 def make_move():
   global board
   player = ['o', 's']
-  print(type(board), 'make move')
   #array of 1's
   boRD = list(board)
   two = -1
@@ -14,10 +14,8 @@ def make_move():
   four = -1
 
   for c in range(7):
-    print("--")
     
     for r in range(6):
-      print(board[r])
       if board[r][c] == 1:
         return c
       else:
@@ -38,9 +36,7 @@ def make_move():
 
 def calculate_move(column):
   global board
-  print(type(board), 'calc move')
-  print("AAAAAAAAA")
-  update_board(column, "o")
+  update_board(column, 'o')
   col = make_move()
   return col
   
@@ -51,9 +47,9 @@ and the board array. Returns board with recalculated weights.
 def update_board(col, player):
   global board
   col = int(col)
-  for i in range(6):
+  for i in range(5, -1, -1):
     if board[i][col] != 's' and board[i][col]!='o':
-      board[i][col]=player
+      board[i][col] = player
       break
 
   recalculate_weight()
@@ -72,7 +68,10 @@ avoid:
 def recalculate_weight():
   global board
   recalculation(1)
-  recalculation(2)
+  #recalculation(2)
+  
+  
+  
 
 '''
 d = dir
@@ -82,34 +81,49 @@ def recalculation(check_for):
   global board
 
   #check rows
-  for row in board:
+  for row_i in range(6):
     for col in range(4):
+      row = board[row_i]
       in_row = 0
-      blank = (0, 0) #check default
+      blank = (-1, -1) #check default
       if row[col]=="s" or row[col] == "o":
         for i in range(1, 4):
           if row[col] == row[col + i]:
             in_row += 1
           else:
-            blank = (row, col+1)
-        f_num = int(float_num(blank))
-        if in_row == 3 and f_num == 0:
-          board[blank[0]][blank[1]] = check_for
-        else:
-          if f_num % 2 == 1: #if odd, play bc smort move
-            board[blank[0]][blank[1]] = 3
-          else: #if even, avoid cuz other can play and make it a 4
-            board[blank[0]][blank[1]] = 5
+            if row[col + i] == ' ':
+              print("blank r, c: v")
+              print((row_i, col + i))
+              blank = (row_i, col+i)
+        if blank[0] != -1:
+          print("blank: v")
+          print(blank)
+          f_num = int(float_num(blank))
+          print("fnum v")
+          print(f_num)
+          if in_row == 3 and f_num == 0:
+            if row[col] == 's':
+              board[int(blank[0])][int(blank[1])] = 1
+            else:
+              board[int(blank[0])][int(blank[1])] = 2
+          else:
+            if f_num % 2 == 1: #if odd, play bc smort move
+              board[int(blank[0])][int(blank[1])] = 3
+            else: #if even, avoid cuz other can play and make it a 4
+              board[int(blank[0])][int(blank[1])] = 5
 
       if row[col] == " ":
-        blank = (row, col)
+        blank = (row_i, col)
         for i in range(1, 4):
           if row[col + 1] == row[col + i] and row[col + 1] != " ":
             in_row += 1
-          else:
-            blank = (row, col+1)
+          
+           
           if in_row == 3 and float_num(blank) == 0:
-            board[blank[0]][blank[1]] = check_for
+            if row[col+1] == 's':
+              board[int(blank[0])][int(blank[1])] = 1
+            else:
+              board[int(blank[0])][int(blank[1])] = 2
             
   #check cols
   for col_i in range(7):
@@ -124,12 +138,17 @@ def recalculation(check_for):
             in_row += 1
         if in_row == 3 and board[row_i+3][col_i]==" ":
             board[0][col_i] = check_for
+  for r in range(6):
+    print(board[r])
  
 '''
 Checks how much it's floating
 '''
 def float_num(blank):
   global board
+  
+
+  print(type(blank))
 
   blank_count = 0
   b_row = int(blank[0])
@@ -166,17 +185,37 @@ async def gameloop (socket, created):
 
     match message[0]:
       case 'GAMESTART':
-        move = 3
-        await socket.send(f'PLAY:{move}')
+        print(created)
+        if created == True:
+          print("AAAAAA")
+
+          move = 3
+          await socket.send(f'PLAY:{move}')
+          update_board(move, 's')
+          
       case'OPPONENT':
         print(message[1])
         print("AAAAAA")
         move = calculate_move(message[1])
+        update_board(move, 's')
+
+        valid = False
+        while valid == False:
+          for row in range(6):
+            if board[row][move]==' ':
+              valid = True
+          if valid!=True:
+            move = random.randint(0, 7)
+
         print(move)
         await socket.send(f'PLAY:{move}')
         
       case 'WIN' | 'LOSS' | 'DRAW' | 'TERMINATED':
         print(message[0])
+
+      case 'ERROR':
+        move = random.randint(0, 7)
+        await socket.send(f'PLAY:{move}')
 
         active = False
 
